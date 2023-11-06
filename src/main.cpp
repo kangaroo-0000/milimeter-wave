90c0c5378e13d91957a5f884ce40872d057f5286
Beckham Pan <beckham.0831@yahoo.com.tw>
2023-11-06 10:24:10 -0800

    add Encoder code

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
 #include <Arduino.h>
+#include <Encoder.h>
 
 const int motor_pin_1 = 5;  // pul+
 const int motor_pin_2 = 4;  // pul-
 const int motor_pin_3 = 3;  // dir+
 const int motor_pin_4 = 2;  // dir-
+const int encoder_pin_1 = 18;
+const int encoder_pin_2 = 19;
+
+Encoder myEnc(encoder_pin_1, encoder_pin_2);
+long oldPosition = -999;
 
 // put function declarations here:
 void stepCW(int steps);
 void stepCCW(int steps);
+void getCurrentPosition();
 
 void setup() {
   // put your setup code here, to run once:
@@ -29,7 +36,16 @@ void loop() {
     }
   }
 }
+
 // put function definitions here:
+void getCurrentPosition() {
+  long newPosition = myEnc.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
+  }
+}
+
 void stepCW(int steps) {
   int i = 0;
   int delay_time = 10;  // experimentally derived delay
@@ -38,21 +54,25 @@ void stepCW(int steps) {
     digitalWrite(motor_pin_2, LOW);
     digitalWrite(motor_pin_3, HIGH);
     digitalWrite(motor_pin_4, LOW);
+    getCurrentPosition();
     delay(delay_time);  // in ms
     digitalWrite(motor_pin_1, LOW);
     digitalWrite(motor_pin_2, HIGH);
     digitalWrite(motor_pin_3, HIGH);
     digitalWrite(motor_pin_4, LOW);
+    getCurrentPosition();
     delay(delay_time);
     digitalWrite(motor_pin_1, LOW);
     digitalWrite(motor_pin_2, HIGH);
     digitalWrite(motor_pin_3, LOW);
     digitalWrite(motor_pin_4, HIGH);
+    getCurrentPosition();
     delay(delay_time);
     digitalWrite(motor_pin_1, HIGH);
     digitalWrite(motor_pin_2, LOW);
     digitalWrite(motor_pin_3, LOW);
     digitalWrite(motor_pin_4, HIGH);
+    getCurrentPosition();
     delay(delay_time);
   }  // each step takes 40ms to complete
 }
@@ -65,21 +85,25 @@ void stepCCW(int steps) {
     digitalWrite(motor_pin_2, LOW);
     digitalWrite(motor_pin_3, LOW);
     digitalWrite(motor_pin_4, HIGH);
+    getCurrentPosition();
     delay(delay_time);
     digitalWrite(motor_pin_1, LOW);
     digitalWrite(motor_pin_2, HIGH);
     digitalWrite(motor_pin_3, LOW);
     digitalWrite(motor_pin_4, HIGH);
+    getCurrentPosition();
     delay(delay_time);
     digitalWrite(motor_pin_1, LOW);
     digitalWrite(motor_pin_2, HIGH);
     digitalWrite(motor_pin_3, HIGH);
     digitalWrite(motor_pin_4, LOW);
+    getCurrentPosition();
     delay(delay_time);
     digitalWrite(motor_pin_1, HIGH);
     digitalWrite(motor_pin_2, LOW);
     digitalWrite(motor_pin_3, HIGH);
     digitalWrite(motor_pin_4, LOW);
+    getCurrentPosition();
     delay(delay_time);
   }  // each step takes 40ms to complete
 }
--- src/serial.py
+++ src/serial.py
@@ -1,5 +1,23 @@
 import serial
 
-ser = serial.Serial('/dev/ttyACM0', 9600) # replace with your port
-ser.write(b'a')
+# Initialize variables
+arm_position = 0  
 
+def update_arm_position_display():
+    # reserved for GUI design
+    pass
+
+def ccw_button_pushed():
+    ser.write(b'a')  # turn arm CCW 0.9 degree
+
+def cw_button_pushed():
+    ser.write(b'b')  # turn arm CW 0.9 degree
+
+ser = serial.Serial('/dev/ttyACM0', 9600) # replace with your port
+
+while True:
+    if ser.in_waiting:
+        encoder_position = ser.readline().decode('utf-8').rstrip()
+        print(f"Position: {encoder_position}")
+        arm_position = float(encoder_position)
+        update_arm_position_display()
